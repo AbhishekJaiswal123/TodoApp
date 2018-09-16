c9d1d7f1a92645c2cb2eb1847a8707c054548ce0
Abhishek Jaiswal <jabhi207@gmail.com>
2018-09-16 17:06:05 +0530

    Added reducer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(rootReducer);
+
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root'));
+
+
+    //registerServiceWorker();
